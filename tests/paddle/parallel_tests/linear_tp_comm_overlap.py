345ce37b5ec2241b97cb6cf5566200ec255e8afe
Anderson Meng <ameng@nvidia.com>
2024-08-05 07:04:09 +0000

    Refactor unittest of Linear on tp-comm-overlap
    Remove duplicated logic and reuse the abstract
     `_TestLinearTpBase` I refined in [0f1763f][1]
    
    [1]: https://github.com/anderson101866/TransformerEngine/commit/0f1763f0e8959f52289edd3116d54705bea84467
    
    Signed-off-by: Anderson Meng <ameng@nvidia.com>

--- tests/paddle/parallel_tests/linear_tp_comm_overlap.py
+++ tests/paddle/parallel_tests/linear_tp_comm_overlap.py
@@ -7,94 +7,10 @@ import unittest
 
 import paddle
 from paddle.distributed import fleet
-from paddle.distributed.fleet.layers.mpu import mp_ops
 
 from utils import assert_allclose, assert_shape, set_random_seed
+from parallel_tests.linear_tp import _TestLinearTpBase
 import transformer_engine.paddle as te
-
-class _TestLinearTpBase(unittest.TestCase):
-    """Tests Linear layer with column/row parallelism in BF16"""
-
-    def setUp(self):
-        self.set_attr()
-        self.init_dist_env()
-        paddle.set_default_dtype(self.global_dtype)
-
-    def init_dist_env(self):
-        """Init Paddle Fleet environment"""
-        strategy = fleet.DistributedStrategy()
-        self.model_parallel_size = 2
-        strategy.hybrid_configs = {
-            "dp_degree": 1,
-            "mp_degree": self.model_parallel_size,
-            "pp_degree": 1,
-        }
-        strategy.hybrid_configs["mp_configs"].need_broadcast_data = False
-        fleet.init(is_collective=True, strategy=strategy)
-        self.rank = fleet.worker_index()
-        self.hcg = fleet.get_hybrid_communicate_group()
-        self.tp_group = self.hcg.get_model_parallel_group()
-        self.world_size = self.hcg.get_model_parallel_world_size()
-
-    def set_attr(self):
-        """Set test configs"""
-        self.batch_size = 16
-        self.in_features = 32
-        self.out_features = 64
-        self.global_dtype = "bfloat16"
-        self.rtol = 1e-3
-        self.atol = 1e-3
-        self.fp8 = False
-        self.sequence_parallel = False
-
-    def _train_one_step(self, layer, inp, optimizer, split_input="none", gather_output=False):
-        inp = paddle.to_tensor(inp, stop_gradient=True)
-        assert split_input in ["none", "column", "row"]
-        if split_input == "column":
-            split_size = inp.shape[1] // self.world_size
-            input_parallel = inp[:, split_size * self.rank : split_size * (self.rank + 1)]
-        elif split_input == "row":
-            split_size = inp.shape[0] // self.world_size
-            input_parallel = inp[split_size * self.rank : split_size * (self.rank + 1), :]
-        else:
-            input_parallel = inp
-        if not input_parallel.is_contiguous():
-            input_parallel = input_parallel.contiguous()
-        input_parallel.stop_gradient = False
-        out = layer(input_parallel)
-        if gather_output:
-            total_out = mp_ops._c_concat(out, group=self.tp_group)
-        else:
-            total_out = out
-        loss = total_out.mean()
-        loss.backward()
-        optimizer.step()
-        optimizer.clear_grad()
-        if split_input != "none":
-            grad_input = []
-            paddle.distributed.all_gather(grad_input, input_parallel.grad, group=self.tp_group)
-            if split_input == "column":
-                grad_input = paddle.concat(grad_input, axis=1)
-            elif split_input == "row":
-                grad_input = paddle.concat(grad_input, axis=0)
-        else:
-            grad_input = input_parallel.grad
-        return loss, grad_input
-
-    def _create_pd_linear(self, layer_te: te.Linear, axis: int=0):
-        """Create a normal Paddle nn.Linear with weight=[in_features, out_features] for comparing result"""
-        layer_pd = te.Linear(
-            self.in_features,
-            self.out_features,
-            backend="paddle",
-        )
-        # Get total weight
-        total_weight = []
-        partial_weight = layer_te.weight.clone().detach()
-        paddle.distributed.all_gather(total_weight, partial_weight, group=self.tp_group)
-        total_weight = paddle.concat(total_weight, axis=axis)
-        layer_pd.weight.copy_(total_weight.T, True)
-        return layer_pd
 
 B = 16
 H = 64
